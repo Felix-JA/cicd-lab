d0b593868e3cccc6d5710b998c06e1fe99799cb2
Felix-JA <felixjarabaa@gmail.com>
2026-04-08 16:43:37 +0000

    primer commit - lab cicd

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', service: 'backend', version: '1.0.0' });
+});
+
+app.get('/api/message', (req, res) => {
+  res.json({ message: 'Hola desde el Backend!', timestamp: new Date() });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend corriendo en puerto ${PORT}`);
+});
